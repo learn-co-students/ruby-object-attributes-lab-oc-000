45c25ccf50b851d576207f145c10f7cd9bdd35a3
robcohen <rc2842@columbia.edu>
2015-11-18 13:28:53 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+
+  def name
+    @name
+  end
+  def name=(name_of_person)
+    @name = name_of_person
+  end
+  def job
+    @job
+  end
+  def job=(job_of_person)
+    @job = job_of_person
+  end
+
+end
